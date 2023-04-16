7c718f38fef04ade4cf595215ecee1527e71ba74
Early <earlylalo1@gmail.com>
2023-04-17 01:46:00 +0800

    Updated

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.shop-now-btn');
 button.addEventListener('click', () => {
   console.log('Button clicked!');
-  window.location.href = 'login.html';
+  window.location.href = 'Furry-Friends.html';
 });
 
